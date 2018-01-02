d084bcb33c1f4276bcfdcf3f3092400f09d4bbb7
Gabrielly de Andrade <gabrielly.andrade@poli.ufrj.br>
2018-01-02 11:40:03 -0200

    * changing python file to export data to a csv file called 'results.csv'

--- src/crawlerSelenium.py
+++ src/crawlerSelenium.py
@@ -5,8 +5,23 @@
 
 #Import the module to webscrape dynamic content from a page
 from selenium import webdriver
+import csv
+
+def csvFile():
+	csvFile = open("../results.csv","w", newline='')
+	file = csv.writer(csvFile)
+	file.writerow(["title","productName","url"])
+	csvFile.close()
+
+def appendCsvFile(title, productName, url):
+	with open("../results.csv","a", newline='') as csvFile:
+		write = csv.writer(csvFile)
+		write.writerow([title , productName , url])
+	csvFile.close()
 
 chromePath = r"C:\Users\Gabri\webDriver\chromedriver.exe"
+
+csvFile()
 
 for numberPage in range (1,3):
 	#Creating the instance of Chrome WebDriver
@@ -17,12 +32,12 @@ for numberPage in range (1,3):
 	assert "Época Cosméticos" in driver.title
 
 	titleList = driver.find_elements_by_xpath("//span[@class='shelf-default__brand']/a")
-	print (titleList) 
+	#print (titleList) 
 	productNameList = driver.find_elements_by_class_name("shelf-default__product-name")
 	urlList = driver.find_elements_by_class_name("shelf-default__link")
 
 	for i in range (len(titleList)):
-		print(titleList[i].text, \
+		appendCsvFile(titleList[i].text, \
 				productNameList[i].text, urlList[i].get_attribute("href"))	
 
 	titleList.clear()
