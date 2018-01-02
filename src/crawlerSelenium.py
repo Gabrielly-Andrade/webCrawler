156cb24fb36e6ddb03f2efa7bc80f917fcbe1f80
Gabrielly de Andrade <gabrielly.andrade@poli.ufrj.br>
2018-01-02 10:42:16 -0200

    Initial commit: Basic code for dynamic content

---
+++ src/crawlerSelenium.py
@@ -0,0 +1,38 @@
+# -*- encoding: utf-8 -*-
+# Created on 2017-12-27 22:24:36
+# Project: epocacosmeticos
+# Author: Gabrielly de Andrade
+
+#Import the module to webscrape dynamic content from a page
+from selenium import webdriver
+
+chromePath = r"C:\Users\Gabri\webDriver\chromedriver.exe"
+
+for numberPage in range (1,3):
+	#Creating the instance of Chrome WebDriver
+	driver = webdriver.Chrome(chromePath)
+	#Navigating to the page given by the URL
+	driver.get("http://www.epocacosmeticos.com.br/selecao/acao#" + str(numberPage))
+	#Confirming that title has "Época Cosméticos" word in it
+	assert "Época Cosméticos" in driver.title
+
+	titleList = driver.find_elements_by_xpath("//span[@class='shelf-default__brand']/a")
+	print (titleList) 
+	productNameList = driver.find_elements_by_class_name("shelf-default__product-name")
+	urlList = driver.find_elements_by_class_name("shelf-default__link")
+
+	for i in range (len(titleList)):
+		print(titleList[i].text, \
+				productNameList[i].text, urlList[i].get_attribute("href"))	
+
+	titleList.clear()
+	driver.close()
+
+	'''
+	Trying to use a automatic pagination, but without success :/ 
+	#for i in range (1,3):
+	driver.find_element_by_xpath("//*[@id='PagerBottom_26687552']/ul/li[8]").click()
+	titleList2 = driver.find_elements_by_xpath("//span[@class='shelf-default__brand']/a") 
+	for title in titleList2:
+		print(title.text)
+	'''
