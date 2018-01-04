ddd77c0a4f6038f5728ad217e90c291da16f0e39
Gabrielly de Andrade <gabrielly.andrade@poli.ufrj.br>
2018-01-04 00:07:37 -0200

    src/crawlerSelenium.py
    * changing python file to implement web scraping for all the categories in the website. Ps. need more tests, only tested with the 'perfumes' page

--- src/crawlerSelenium.py
+++ src/crawlerSelenium.py
@@ -13,7 +13,7 @@ def csvFile():
 	file.writerow(["title","productName","url"])
 	csvFile.close()
 
-def appendCsvFile(titleList, productNameList, urlList):
+def appendCSVFile(titleList, productNameList, urlList):
 	for i in range (len(titleList)):
 		with open("../results.csv","a", newline='') as csvFile:
 			write = csv.writer(csvFile)
@@ -22,35 +22,58 @@ def appendCsvFile(titleList, productNameList, urlList):
 				urlList[i].get_attribute("href")])
 		csvFile.close()
 
-def navigation(url, numberPage):
+def categoryPage(categoryURL, numberPage):
+	#This function is responsible for navigate to a page given
+	#with the main url (url category page) and the number
+	#of the page (or section)
 	global driver
-	driver = webdriver.Chrome()
-	driver.get(url + str(numberPage))
+	driver = webdriver.Firefox()
+	driver.get(categoryURL + "#" + str(numberPage))
 
 def extractFromURL():
-	global driver
-		
+	#This function extract the title, product name and url 
+	#from each product of the page given
+	global driver	
 	titleList = driver.find_elements_by_xpath("//span[@class='shelf-default__brand']/a")
 	productNameList = driver.find_elements_by_class_name("shelf-default__item")
 	urlList = driver.find_elements_by_class_name("shelf-default__link")
-
-	appendCsvFile(titleList, productNameList, urlList)
+	appendCSVFile(titleList, productNameList, urlList)
 	driver.close()	
 
-def setUp(url):
+def setUp(categoryURL):
+	#This function control the number (section or navigation) of each  
+	#category page and create a loop to call other auxiliary functions
 	global driver
 	numberPage = 1
 	while True:
-		navigation(url, numberPage)
+		categoryPage(categoryURL, numberPage)
 		if not driver.find_elements_by_class_name("shelf-default__item"):
 			driver.close()
 			break
 		extractFromURL()			
 		numberPage += 1
 
+def getLinks(mainURL):
+	#This function get all the links in the category section
+	#and return a list of pages to scrape	
+	global driver
+	driver = webdriver.Firefox()
+	driver.get(mainURL)
+	pages = []
+	pageList = driver.find_elements_by_xpath("//ul[@class='menu__list']/li[1]/div/ul/li/a")
+	for i in range (0,len(pageList)):
+		pages.append(pageList[i].get_attribute("href"))
+	driver.close()
+	return pages
+
 def main ():
 	csvFile()
-	setUp("http://www.epocacosmeticos.com.br/selecao/acao#")
+	pages = getLinks("http://www.epocacosmeticos.com.br/")
+	print (pages)
+	#for i in range (0,len(pages)): -> not tested yet
+	for i in range (0,1): #Testing only the "perfume" page
+		setUp(pages[i])
+
 
 if __name__ == '__main__':
     main()
