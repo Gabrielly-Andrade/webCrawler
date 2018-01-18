a2168706c9174991ab95369d1b3942167fdd529f
Gabrielly de Andrade <gabrielly.andrade@poli.ufrj.br>
2018-01-18 10:11:02 -0200

    * changing crawler file - adding class Crawler

--- src/crawler.py
+++ src/crawler.py
@@ -36,9 +36,43 @@ class ExportCsv:
             write.writerow([self.title, self.name, self.url])
 
 
+class Crawler:
+
+    def __init__(self):
+        self.driver = webdriver.Firefox()
+        self.driver.maximize_window()
+
+    def get_categories(self, main_url):
+        self.driver.get(main_url)
+        categories_pages = []
+        page_list = self.driver.find_elements_by_xpath(
+            "//ul[@class='menu__list']/li[1]/div/ul/li/a")
+        for page in page_list:
+            url = page.get_attribute("href")
+            if "ofertas" not in url:
+                categories_pages.append(url)
+        self.driver.quit()
+        self.visit_categories(categories_pages)
+
+    def visit_categories(self, categories_pages):
+        for page in categories_pages:
+            number_page = 1
+            while True:
+                self.__init__()
+                self.driver.get(page + "#" + str(number_page))
+                if not self.driver.find_elements_by_class_name(
+                        "shelf-default__item"):
+                    self.driver.quit()
+                    break
+                extract_products()
+                number_page += 1
+
 def main():
     csv_file = ExportCsv("../results.csv")
     csv_file.create_file()
+
+    epoca_cosmeticos = Crawler()
+    epoca_cosmeticos.get_categories("http://www.epocacosmeticos.com.br/")
 
 
 if __name__ == '__main__':
